3f948ffd9d8dde9faaebed1a18b91dffd5baadd2
Jason Gross <jgross@mit.edu>
2014-05-02 15:47:08 -0400

    Add basic ui Signed-off-by:Jason Gross <jgross@mit.edu>

--- src/geometricpacking/GeometricPacking.java
+++ src/geometricpacking/GeometricPacking.java
@@ -16,7 +16,7 @@ public class GeometricPacking {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        geometricpacking.ui.Main.main(args);
     }
 
 }
